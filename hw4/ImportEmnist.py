5a037c4d367c5da9a6c3342248ae85cfdb7cf136
tojose <tojose@rmplc.net>
2018-06-19 17:14:11 +0530

    download emnist

--- hw4/ImportEmnist.py
+++ hw4/ImportEmnist.py
@@ -1,12 +1,14 @@
 import numpy as np
 import cv2
-import cntk cnt
-import urllib
+import cntk as cnt
+import urllib.request
 import zipfile
 import csv
 import re
 import gzip
+import struct
 import sys
+import os
 
 try:
     from urllib.request import urlretrieve
@@ -18,5 +20,65 @@ def progress(count, blockSize, totalSize):
     sys.stdout.write("\r%d%%" % percent + ' complete')
     sys.stdout.flush()
 
-print("Start download EMNIST data sets")
+def loadData(src, cimg):
+    print ('Loading ' + src)
+    gzfname, h = urlretrieve(src, './delete.me')
+    print ('Done.')
+    try:
+        with gzip.open(gzfname) as gz:
+            n = struct.unpack('I', gz.read(4))
+            # Read magic number.
+            if n[0] != 0x3080000:
+                raise Exception('Invalid file: unexpected magic number.')
+            # Read number of entries.
+            n = struct.unpack('>I', gz.read(4))[0]
+            print(n)
+            if n != cimg:
+                raise Exception('Invalid file: expected {0} entries.'.format(cimg))
+            crow = struct.unpack('>I', gz.read(4))[0]
+            ccol = struct.unpack('>I', gz.read(4))[0]
+            if crow != 28 or ccol != 28:
+                raise Exception('Invalid file: expected 28 rows/cols per image.')
+            # Read data.
+            res = np.fromstring(gz.read(cimg * crow * ccol), dtype = np.uint8)
+    finally:
+        os.remove(gzfname)
+    return res.reshape((cimg, crow * ccol))
 
+def loadLabels(src, cimg):
+    print ('Loading ' + src)
+    gzfname, h = urlretrieve(src, './delete.me')
+    print ('Done.')
+    try:
+        with gzip.open(gzfname) as gz:
+            n = struct.unpack('I', gz.read(4))
+            # Read magic number.
+            if n[0] != 0x1080000:
+                raise Exception('Invalid file: unexpected magic number.')
+            # Read number of entries.
+            n = struct.unpack('>I', gz.read(4))
+            if n[0] != cimg:
+                raise Exception('Invalid file: expected {0} rows.'.format(cimg))
+            # Read labels.
+            res = np.fromstring(gz.read(cimg), dtype = np.uint8)
+    finally:
+        os.remove(gzfname)
+    return res.reshape((cimg, 1))
+
+def getStack(dataSrc, labelsSrc, cimg):
+    data = loadData(dataSrc, cimg)
+    labels = loadLabels(labelsSrc, cimg)
+    return np.hstack((data, labels))
+
+def importEmnist():
+    urllib.request.urlretrieve("http://biometrics.nist.gov/cs_links/EMNIST/gzip.zip", "/tmp/file.zip", reporthook=progress)
+    zip_ref = zipfile.ZipFile('/tmp/file.zip', 'r')
+    zip_ref.extractall('/tmp/')
+    zip_ref.close()    
+
+def main():
+    importEmnist()
+
+if __name__ == "__main__":
+    print("Start import EMNIST...")
+    importEmnist()
