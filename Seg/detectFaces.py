c73f0b6001a550707f8f38f2194575faeb76228a
tojose <tojose@rmplc.net>
2018-06-25 17:48:54 +0530

    face detection

--- Seg/detectFaces.py
+++ Seg/detectFaces.py
@@ -1,8 +1,12 @@
-import cv2
+import cv2, numpy as np
+import imutils
 import sys
+import image_resize as ir
+import image_eqhist as eh
 
 def detectFace(image, cascade):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  
+    img_copy = np.copy(image)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     
     face_cascade = cv2.CascadeClassifier(cascade)
 
@@ -10,22 +14,32 @@ def detectFace(image, cascade):
 
     if (len(faces) == 0):
         return None, None
+    else:
+        print ("Faces found ", len(faces))
 
-    (x, y, w, h) = faces[0]
-    return gray[y:y+w, x:x+h], faces[0]
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img_copy, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    return img_copy
+
+def pre_process(image):
+    equalized = eh.eqhist(image)
+    return equalized
 
 def main():
     pass
 
 if __name__ == "__main__":
-    image = "./images/IMG_20180625_145128.jpg"  # sys.argv[1]
-
-    img = cv2.imread(image)
 
     hcasc = "haarcascade_frontalface_default.xml"
 
+    image = "./test_images/t2.jpg"  # sys.argv[1]
+    
+    img = cv2.imread(image)
+    img = ir.resize(img, width=1200)
+
     detected = detectFace(img, hcasc)
 
-    cv2.imshow(detected)
+    cv2.imshow('detected', detected)
     cv2.waitKey(0)
 
