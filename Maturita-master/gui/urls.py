c8118e98c6060aca2212fb6bed404cde2475f2f9
tojose <tojose@rmplc.net>
2019-01-09 20:09:10 +0530

    cleaned

--- Maturita-master/gui/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^download.html$', views.download, name='download'),
-    url(r'^read.html$', views.read, name='read'),
-    url(r'^info.html$', views.show_info, name='info')
-]
